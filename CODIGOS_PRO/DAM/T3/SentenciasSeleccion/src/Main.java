5fac8182e5a05c4ae518071e1e05f62d59629728
josanmontalban <josanmontalban@gmail.com>
2025-10-29 13:40:58 +0100

    commit

--- CODIGOS_PRO/DAM/T3/SentenciasSeleccion/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SentenciaIF sentenciaIF = new SentenciaIF();
-        // sentenciaIF.estaAprobado(1);;
-        sentenciaIF.notaExamen(8);
-    }
-}
